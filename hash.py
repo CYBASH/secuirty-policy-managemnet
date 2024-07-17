33d5384020a032e07ccac10e144252033cb82ce2
SATISH BABU THOTA <22ktsatish5b6@gmail.com>
2024-07-17 13:09:38 +0000

    Hashing

---
+++ hash.py
@@ -0,0 +1,40 @@
+import hashlib as h
+
+class Password:
+    def __init__(self, user_Password):
+        self.user_Password = user_Password
+
+    #checking the password is valid or not
+
+    def validation(self):
+        if len(self.user_Password) < 8:
+            return False
+        elif not self.has_capital_letter():
+            return False
+        elif not self.has_lower_letter():
+            return False
+        elif not self.has_special_char():
+            return False
+        else:
+            self.encrypt(self.user_Password)
+            return True
+
+    #checking the password cotaining atleast a uppercase letter
+    def has_capital_letter(self):
+        return any(char.isupper() for char in self.user_Password)
+
+    #checking the password cotaining atleast a lowercase letter        
+    def has_lower_letter(self):
+        return any(char.islower() for char in self.user_Password)
+
+    #checking the password cotaining atleast a special character
+    def has_special_char(self):
+    	special_char="!@#$%^&*()_-+={}[]?/.,';:|~`"
+    	return any(char in special_char for char in self.user_Password)
+
+    #encrypting the password to hash codes
+    def encrypt(self, password):
+    	hashing=h.new('SHA256')
+    	hashing.update(password.encode())
+    	print("Encrypting password:",hashing.hexdigest())
+
