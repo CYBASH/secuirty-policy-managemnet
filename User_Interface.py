bbe654f69966cc39f17b0a030e9a01c004c82e68
Sivaram-C5 <sivaramgajivelli@gmail.com>
2024-07-19 23:05:51 +0530

    Updated UI

--- User_Interface.py
+++ User_Interface.py
@@ -4,19 +4,28 @@ import webbrowser
 root = Tk() 
 #Title
 root.title("Security Policy Management Project")
+# root.configure(bg="#505050")
+bg = PhotoImage(file="background.jpeg")
 root.geometry('500x500')
 
-def clicked():
-    webbrowser.open('file:///C:\Users\sivay\Desktop\Security_Policy_Management\secuirty-policy-managemnet\sample_info.html')
+def project_info():
+    webbrowser.open('file:///C:/Users/sivay/Desktop/Security%20Policy%20Management/secuirty-policy-managemnet/sample_info.html')
 
-btn = Button(root, text = "Button", fg = "red", command=clicked)
-btn.pack()
+btn = Button(root, 
+             text = "Project Info",
+             bg="blue",
+             fg = "white", 
+             font=("Verdana", 11, "bold"),
+             padx=4,
+             command=project_info)
+btn.pack(pady=20)
+
 a = Label(root, 
-          bg="#D3D3D3", 
+          bg="white", 
           fg="black", 
           padx=20,
           text="Security Policy Management Project",
-          font=("Helvetica", 12, "bold"))
+          font=("Verdana", 14, "bold"))
 a.pack()
 
 
