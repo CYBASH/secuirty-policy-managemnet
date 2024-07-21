90ddc545531f2300653525ffd212a725dfc6f36a
Sivaram-C5 <sivaramgajivelli@gmail.com>
2024-07-21 16:22:16 +0530

    Embeded User_Interface and Policy_Interface into classes

--- Policy_Interface.py
+++ Policy_Interface.py
@@ -1,113 +1,116 @@
 from tkinter import *
 
-#UI Starter
-def startUI():
-    root.mainloop()
+class Policies_Interface:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Security Policy Management Project")
+        self.root.configure(bg="#505050")
+        self.root.geometry('500x500')
+        self.root.resizable(False, False)
 
-#add policy btn definition
-def add_policy():
-    print("Add Policy Btn clicked")
+        #Policy Name
+        self.policy_label = Label(self.root, text="Policy Name:", padx=5, font=("Verdana", 11))
+        self.policy_label.grid(row=0, column=0, padx=(100, 10), pady=(25, 10), sticky=E)  
+        self.policy_entry = Entry(self.root)
+        self.policy_entry.grid(row=0, column=1, padx=(10, 100), pady=(25, 10), sticky=W) 
 
-#update policy btn definition
-def update_policy():
-    print("Update Policy Btn clicked")
+        #Description
+        self.desc_label = Label(self.root, text="Description:", padx=10, font=("Verdana", 11))
+        self.desc_label.grid(row=1, column=0, padx=(100, 10), pady=10, sticky=E)  
+        self.desc_entry = Entry(self.root)
+        self.desc_entry.grid(row=1, column=1, padx=(10, 100), pady=10, sticky=W) 
 
-#load text file
-def load_file():
-    policies = []
-    with open("policies.txt", "r") as file:
-        policies = file.readlines()
-        policies_list.config(state="normal")
-        policies_list.delete("1.0", END)
-        for policy in policies:
-            policies_list.insert(END, policy) 
-        policies_list.config(state="disabled")  
+        #Add_policy Btn
+        add_policy_btn = Button(self.root,
+                    text="Add Policy",
+                    bg="white",
+                    fg="black",
+                    font=("Verdana", 11),
+                    padx=2,
+                    command=self.add_policy)
+        add_policy_btn.grid(row=2, column=0, padx=20, pady=10, sticky=E) 
 
-#enforce policy btn definition
-def enforce_policy():
-    print("Enforce Policy Btn clicked")
+        #Update_policy btn
+        self.update_policy_btn = Button(self.root,
+                    text="Update Policy",
+                    bg="white",
+                    fg="black",
+                    font=("Verdana", 11),
+                    padx=4,
+                    command=self.update_policy)
+        self.update_policy_btn.grid(row=2, column=1, padx=20, pady=10, sticky=W)
 
-#Logout btn definition
-def logout():
-    print("Logout Btn clicked")        
+        #Policies_list
+        self.policies_frame = Frame(self.root)
+        self.policies_frame.grid(row=3, column=0, columnspan=2, padx=20, pady=10, sticky=NSEW)
+        self.policies_list = Text(self.policies_frame, wrap="none", font=("Verdana", 11), height=10)
+        self.policies_list.grid(row=0, column=0, sticky=NSEW)
+        self.yscrollbar = Scrollbar(self.policies_frame, orient="vertical", command=self.policies_list.yview)
+        self.yscrollbar.grid(row=0, column=1, sticky=NS)
+        self.xscrollbar = Scrollbar(self.policies_frame, orient="horizontal", command=self.policies_list.xview)
+        self.xscrollbar.grid(row=1, column=0, sticky=EW)
+        self.policies_list.config(yscrollcommand=self.yscrollbar.set, xscrollcommand=self.xscrollbar.set)
 
-#Window dim specification
-root = Tk()
-root.title("Security Policy Management Project")
-root.configure(bg="#505050")
-root.geometry('500x500')
-root.resizable(False, False)
+        self.root.grid_rowconfigure(3, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=5)
+        self.root.grid_rowconfigure(4, weight=5)
+        self.policies_frame.grid_rowconfigure(0, weight=1)
+        self.policies_frame.grid_columnconfigure(0, weight=1)
 
-#Policy Name
-policy_label = Label(root, text="Policy Name:", padx=5, font=("Verdana", 11))
-policy_label.grid(row=0, column=0, padx=(100, 10), pady=(25, 10), sticky=E)  
-policy_entry = Entry(root)
-policy_entry.grid(row=0, column=1, padx=(10, 100), pady=(25, 10), sticky=W) 
+        self.load_file()
 
-#Description
-desc_label = Label(root, text="Description:", padx=10, font=("Verdana", 11))
-desc_label.grid(row=1, column=0, padx=(100, 10), pady=10, sticky=E)  
-desc_entry = Entry(root)
-desc_entry.grid(row=1, column=1, padx=(10, 100), pady=10, sticky=W) 
+        #Enforce Policy Btn
+        self.enforce_policy_btn = Button(self.root,
+                    text="Enforce Policy",
+                    bg="white",
+                    fg="black",
+                    font=("Verdana", 11),
+                    padx=4,
+                    command=self.enforce_policy)
+        self.enforce_policy_btn.grid(row=4, column=0, padx=20, pady=5, columnspan=2)
 
-#Add_policy Btn
-add_policy_btn = Button(root,
-             text="Add Policy",
-             bg="white",
-             fg="black",
-             font=("Verdana", 11),
-             padx=2,
-             command=add_policy)
-add_policy_btn.grid(row=2, column=0, padx=20, pady=10, sticky=E) 
+        #Logout Btn
+        self.logout_btn = Button(self.root,
+                    text="Logout",
+                    bg="white",
+                    fg="black",
+                    font=("Verdana", 11),
+                    padx=4,
+                    command=self.logout)
+        self.logout_btn.grid(row=5, column=0, padx=20, pady=(0,40), columnspan=2, sticky=N)
+    
+    def startUI(self):
+        self.root.mainloop()
 
-#Update_policy btn
-update_policy_btn = Button(root,
-             text="Update Policy",
-             bg="white",
-             fg="black",
-             font=("Verdana", 11),
-             padx=4,
-             command=update_policy)
-update_policy_btn.grid(row=2, column=1, padx=20, pady=10, sticky=W)
+    #add policy btn definition
+    def add_policy(self):
+        print("Add Policy Btn clicked")
 
-#Policies_list
-policies_frame = Frame(root)
-policies_frame.grid(row=3, column=0, columnspan=2, padx=20, pady=10, sticky=NSEW)
-policies_list = Text(policies_frame, wrap="none", font=("Verdana", 11), height=10)
-policies_list.grid(row=0, column=0, sticky=NSEW)
-yscrollbar = Scrollbar(policies_frame, orient="vertical", command=policies_list.yview)
-yscrollbar.grid(row=0, column=1, sticky=NS)
-xscrollbar = Scrollbar(policies_frame, orient="horizontal", command=policies_list.xview)
-xscrollbar.grid(row=1, column=0, sticky=EW)
-policies_list.config(yscrollcommand=yscrollbar.set, xscrollcommand=xscrollbar.set)
+    #update policy btn definition
+    def update_policy(self):
+        print("Update Policy Btn clicked")
 
-root.grid_rowconfigure(3, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=5)
-root.grid_rowconfigure(4, weight=5)
-policies_frame.grid_rowconfigure(0, weight=1)
-policies_frame.grid_columnconfigure(0, weight=1)
+    #load text file
+    def load_file(self):
+        policies = []
+        with open("policies.txt", "r") as file:
+            self.policies = file.readlines()
+            self.policies_list.config(state="normal")
+            self.policies_list.delete("1.0", END)
+            for self.policy in self.policies:
+                self.policies_list.insert(END, self.policy) 
+            self.policies_list.config(state="disabled")  
 
-load_file()
+    #enforce policy btn definition
+    def enforce_policy(self):
+        print("Enforce Policy Btn clicked")
 
-#Enforce Policy Btn
-enforce_policy_btn = Button(root,
-             text="Enforce Policy",
-             bg="white",
-             fg="black",
-             font=("Verdana", 11),
-             padx=4,
-             command=enforce_policy)
-enforce_policy_btn.grid(row=4, column=0, padx=20, pady=5, columnspan=2)
+    #Logout btn definition
+    def logout(self):
+        print("Logout Btn clicked")     
 
-#Logout Btn
-logout_btn = Button(root,
-             text="Logout",
-             bg="white",
-             fg="black",
-             font=("Verdana", 11),
-             padx=4,
-             command=logout)
-logout_btn.grid(row=5, column=0, padx=20, pady=(0,40), columnspan=2, sticky=N)
-
-startUI()
+if __name__ == "__main__":
+    root = Tk()
+    app = Policies_Interface(root)
+    app.startUI()  
--- User_Interface.py
+++ User_Interface.py
@@ -4,110 +4,115 @@ from securitypolicy import *
 from hash import *
 from ProjectInfo import *
 
-#Login Btn Definition
-def login():
-    print("Login Btn CLicked")
+class User_Interface:
+    #Login Btn Definition
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Security Policy Management Project")
+        self.root.configure(bg="#505050")
+        self.root.geometry('500x500')
+        self.root.resizable(False, False)
 
-    p = Password(password_entry.get())
-    p.encrypt()
+        #Project Info Button 
+        self.info_btn = Button(self.root,
+                    text="Project Info",
+                    bg="blue",
+                    fg="white",
+                    font=("Verdana", 11, "bold"),
+                    padx=4,
+                    command=self.project_info)
+        self.info_btn.grid(row=0, column=0, padx=20, pady=20, columnspan=2) 
 
-    d = Database()
-    if (p.getHashCode()) == (d.getPassword(username_entry.get())):
-        print("Logged in")
-    else:
-        print("Wrong password")
+        #Project Heading
+        self.heading = Label(self.root,
+                bg="white",
+                fg="black",
+                padx=20,
+                text="Security Policy Management Project",
+                font=("Verdana", 14, "bold"))
+        self.heading.grid(row=1, column=0, columnspan=2, padx=40, pady=10) 
 
-#Signup Btn Definition   
-def signup():
-    print("Sign Up Btn CLicked")
+        #Picture
+        self.image = Image.open("Security_Policy_manage_pic.png")
+        self.dim = (200, 200)
+        self.adj_img = self.image.resize(self.dim)
+        self.photo = ImageTk.PhotoImage(self.adj_img)
+        self.image_label = Label(self.root, bg="#505050", image=self.photo)
+        self.image_label.grid(row=2, column=0, padx=50, pady=10, columnspan=2, sticky=EW)
 
-    d = Database()
-    d.insertCredentials(username_entry.get(), password_entry.get()) #also need to pass email_entry.get()
+        #username
+        self.username_label = Label(self.root, text="Username:", padx=9, font=("Verdana", 11, "bold"))
+        self.username_label.grid(row=3, column=0, padx=40, pady=10, sticky=E)  
+        self.username_entry = Entry(self.root)
+        self.username_entry.grid(row=3, column=1, padx=40, pady=10, sticky=W)  
 
-#Project Info Btn definition
-def project_info():
-    html_content = """
-        <html>
-            <head>
-                <title>Project Info</title>
-            </head>
-        <body>
-            <h1>Project Info</h1>
-                <p>This is a sample project info page.</p>
-            </body>
-        </html>
-    """
+        #password
+        self.password_label = Label(self.root, text="Password:", padx=10, font=("Verdana", 11, "bold"))
+        self.password_label.grid(row=4, column=0, padx=40, pady=10, sticky=E)  
+        self.password_entry = Entry(self.root, show="*")
+        self.password_entry.grid(row=4, column=1, padx=40, pady=10, sticky=W)
 
-    project_info = ProjectInfo(html_content)
-    project_info.openTempFileInThread()
+        #Login Btn
+        self.login_btn = Button(self.root,
+                    text="Login",
+                    bg="blue",
+                    fg="white",
+                    font=("Verdana", 11, "bold"),
+                    padx=4,
+                    command=self.login) #Command to be defined
+        self.login_btn.grid(row=5, column=1, padx=20, pady=10, columnspan=2, sticky=W)
 
-#Start UI
-def startUI():
-    root.mainloop()
+        #signup btn
+        self.signup_btn = Button(self.root,
+                    text="Sign Up",
+                    bg="blue",
+                    fg="white",
+                    font=("Verdana", 11, "bold"),
+                    padx=4,
+                    command=self.signup) #Command to be defined
+        self.signup_btn.grid(row=5, column=0, padx=150, pady=10, columnspan=2, sticky=W)
 
-root = Tk()
-root.title("Security Policy Management Project")
-root.configure(bg="#505050")
-root.geometry('500x500')
-root.resizable(False, False)
+    #Start UI
+    def startUI(self):
+        self.root.mainloop()
 
-#Project Info Button 
-info_btn = Button(root,
-             text="Project Info",
-             bg="blue",
-             fg="white",
-             font=("Verdana", 11, "bold"),
-             padx=4,
-             command=project_info)
-info_btn.grid(row=0, column=0, padx=20, pady=20, columnspan=2) 
+    def login(self):
+        print("Login Btn CLicked")
 
-#Project Heading
-a = Label(root,
-          bg="white",
-          fg="black",
-          padx=20,
-          text="Security Policy Management Project",
-          font=("Verdana", 14, "bold"))
-a.grid(row=1, column=0, columnspan=2, padx=40, pady=10) 
+        self.p = Password(self.password_entry.get())
+        self.p.encrypt()
 
-#Picture
-image = Image.open("Security_Policy_manage_pic.png")
-dim = (200, 200)
-adj_img = image.resize(dim)
-photo = ImageTk.PhotoImage(adj_img)
-image_label = Label(root, bg="#505050", image=photo)
-image_label.grid(row=2, column=0, padx=50, pady=10, columnspan=2, sticky=EW)
+        self.d = Database()
+        if (self.p.getHashCode()) == (self.d.getPassword(self.username_entry.get())):
+            print("Logged in")
+        else:
+            print("Wrong password")
 
-#username
-username_label = Label(root, text="Username:", padx=9, font=("Verdana", 11, "bold"))
-username_label.grid(row=3, column=0, padx=40, pady=10, sticky=E)  
-username_entry = Entry(root)
-username_entry.grid(row=3, column=1, padx=40, pady=10, sticky=W)  
+    #Signup Btn Definition   
+    def signup(self):
+        print("Sign Up Btn CLicked")
 
-#password
-password_label = Label(root, text="Password:", padx=10, font=("Verdana", 11, "bold"))
-password_label.grid(row=4, column=0, padx=40, pady=10, sticky=E)  
-password_entry = Entry(root, show="*")
-password_entry.grid(row=4, column=1, padx=40, pady=10, sticky=W)
+        self.d = Database()
+        self.d.insertCredentials(self.username_entry.get(), self.password_entry.get()) #also need to pass email_entry.get()
 
-#Login Btn
-login_btn = Button(root,
-             text="Login",
-             bg="blue",
-             fg="white",
-             font=("Verdana", 11, "bold"),
-             padx=4,
-             command=login) #Command to be defined
-login_btn.grid(row=5, column=1, padx=20, pady=10, columnspan=2, sticky=W)
+    #Project Info Btn definition
+    def project_info(self):
+        self.html_content = """
+            <html>
+                <head>
+                    <title>Project Info</title>
+                </head>
+            <body>
+                <h1>Project Info</h1>
+                    <p>This is a sample project info page.</p>
+                </body>
+            </html>
+        """
 
-#signup btn
-signup_btn = Button(root,
-             text="Sign Up",
-             bg="blue",
-             fg="white",
-             font=("Verdana", 11, "bold"),
-             padx=4,
-             command=signup) #Command to be defined
-signup_btn.grid(row=5, column=0, padx=150, pady=10, columnspan=2, sticky=W)
+        self.project_info = ProjectInfo(self.html_content)
+        self.project_info.openTempFileInThread()
 
-startUI()
+if __name__ == "__main__":
+    root = Tk()
+    app = User_Interface(root)
+    app.startUI()  
